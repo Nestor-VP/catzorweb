08366ccc0a02a20a78697048caa6a65f50069f5a
Nestor-VP <112405646+Nestor-VP@users.noreply.github.com>
2023-12-27 01:00:20 +0100

    work2 - leaderboard - integrando tablas y mysql

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
 const express = require('express');
+const mysql = require('mysql2');
 const app = express();
 const puerto = 3000;
+require('dotenv').config();
 
 
+// Configuración de la conexión a MySQL
+const connection = mysql.createConnection({
+    host: process.env.HOSTNAME_DB,
+    user: process.env.USER_DB,
+    password: process.env.PASSWORD_DB,
+    database: process.env.DATABASE,
+  });
+
+const db = connection.promise();
+
+
+
+  // Conectar a la base de datos
+  connection.connect(err => {
+    if (err) {
+      console.error('Error al conectar a MySQL:', err);
+      return;
+    }
+    console.log('Conectado a MySQL');
+  });
+
+
+  // declarando variable para los rankings ( single y team)
+  let leaderboards;
+
+  app.use(express.json());
+
+app.use('/js',express.static('js'));
+
+app.use('/css',express.static('css'));
+
+app.use('/img',express.static('img'));
 
 // Ruta para servir la página HTML
 app.get('/', (req, res) => {
@@ -23,11 +57,45 @@ app.get('/ranks', (req, res) => {
     res.sendFile(__dirname + '/public/ranks.html');
   });
 
-app.use('/js',express.static('js'));
 
-app.use('/css',express.static('css'));
+// Ruta para manejar el formulario de busqueda server
+app.get('/server', async (req, res) => {
 
-app.use('/img',express.static('img'));
+    const numero = req.query.id;
+
+    const league = "league_" + numero;
+
+    query1 = `SELECT aoe_name, country, elo_single, rank_single, verify FROM ${league} ORDER BY rank_single`;
+
+    query2 = `SELECT aoe_name, country, elo_team, rank_team, verify FROM ${league} ORDER BY rank_team`;
+  
+    query3 = `SELECT server_name FROM servers_list WHERE server_id =  ${numero}`;
+
+    try
+  { 
+    const [single_rank,fields1] = await db.query(query1);
+
+    const [team_rank,fields2] = await db.query(query2);
+
+    const [server_name,fields3] = await db.query(query3);
+
+
+
+    leaderboards = { rank_single: single_rank, rank_team:team_rank, server_name: server_name};
+
+        // Mostrar resultados
+        res.json(leaderboards);
+  }
+  catch(err)
+  {
+    console.error('Error al ejecutar la consulta:', err);
+    res.status(500).send('Error al obtener datos de MySQL');
+  }
+
+
+});
+
+
 
 
 
--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,10 +1,157 @@
 
-    function showElements() {
-      var div = document.getElementById('buttons');
-      div.style.display = 'flex';
-      div = document.getElementById('user-search-bar');
-      div.style.display = 'flex';
+document.addEventListener('DOMContentLoaded', function() {
+    // Obtén el ID de la URL
+    var urlParams = new URLSearchParams(window.location.search);
+    var idFromUrl = urlParams.get('id');
 
-      // Lógica para enviar el formulario
-    document.getElementById('miFormulario').submit();
+    // Si hay un ID en la URL, llénalo en el campo de entrada y realiza la búsqueda
+    if (idFromUrl) {
+        document.getElementById('numero').value = idFromUrl;
+        buscarPorId();
     }
+    else {
+        // Si no se proporciona un ID en la URL, muestra un mensaje
+        document.getElementById('mensaje').innerHTML = "Enter Server ID";
+    }
+});
+
+var formulario = document.getElementById("miFormulario");
+
+formulario.addEventListener("submit", function(event) {
+    event.preventDefault();
+    buscarPorId();
+});
+
+function buscarPorId() {
+    var obj = document.getElementById('numero').value;
+    var url = `http://localhost:3000/server?id=${encodeURIComponent(obj)}`;
+
+    
+
+    var btn1 = document.querySelector('#btn_rank1');
+    var btn2 = document.querySelector('#btn_rank2');
+    var buttons = document.querySelector('#buttons');
+
+    var tabla = document.getElementById("ranking_single");
+    var tbody = tabla.getElementsByTagName("tbody")[0];
+
+    var tabla = document.getElementById("ranking_team");
+    var tbody2 = tabla.getElementsByTagName("tbody")[0];
+
+    fetch(url, {
+        method: "GET",
+        headers: { "Content-Type": "application/json"},
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Server ${obj} not found`);
+        }
+
+        buttons.style.display = 'flex';
+
+        return response.json();
+    })
+    .then(data => {
+
+       // console.log(data);
+
+        // Limpia el contenido actual de la tabla
+        tbody.innerHTML = "";
+        tbody2.innerHTML = "";
+        document.getElementById('mensaje').innerHTML = "";
+        document.getElementById('server_name').innerHTML = "";
+
+        const rank_single = data.rank_single;
+        const rank_team = data.rank_team;
+        const server_name = data.server_name[0].server_name;
+
+
+        //console.log(rank_single);
+
+        document.getElementById('server_name').innerHTML = "Server: " +server_name;
+
+
+        
+
+        // Recorre el JSON y agrega filas a la tabla
+
+        // Filling Elo Single leaderboard
+        rank_single.forEach(function(item) {
+            var fila = tbody.insertRow();
+            var rank_cell = fila.insertCell(0);
+            var elo_cell = fila.insertCell(1);
+            var name_cell = fila.insertCell(2);
+            
+
+            // Llena las celdas con los valores del JSON
+            const ver_status = item.verify;
+            let status_simbol = "❌";
+
+            if(ver_status == 1){ status_simbol = "☑️";}
+
+            name_cell.innerHTML = item.aoe_name + status_simbol;
+            
+            elo_cell.innerHTML = item.elo_single;
+            rank_cell.innerHTML = item.rank_single;
+        });
+
+        // Filling Elo Team leaderboard
+        rank_team.forEach(function(item) {
+            var fila = tbody2.insertRow();
+            var rank_cell = fila.insertCell(0);
+            var elo_cell = fila.insertCell(1);
+            var name_cell = fila.insertCell(2);
+            
+
+            // Llena las celdas con los valores del JSON
+            const ver_status = item.verify;
+            let status_simbol = "❌";
+
+            if(ver_status == 1){ status_simbol = "☑️";} 
+
+            name_cell.innerHTML = item.aoe_name + status_simbol;
+            
+            elo_cell.innerHTML = item.elo_team;
+            rank_cell.innerHTML = item.rank_team;
+        });
+
+        // Show Rank_single as default
+        Show_rank_single();
+
+        // Después de completar la solicitud AJAX, actualiza el URL sin recargar la página
+        history.pushState({}, '', `/?id=${obj}`);
+    })
+    .catch(error => {
+        // Muestra un mensaje si ocurre un error
+        document.getElementById('mensaje').innerHTML = error.message;
+        console.error('Error al realizar la operación:', error);
+    });
+}
+
+function Show_rank_single()
+{
+   var ranking_single = document.querySelector('#ranking_single');
+   var ranking_team = document.querySelector('#ranking_team');
+   var message = document.getElementById('mensaje');
+
+   message.innerHTML = "Random map 1vs1 - Leaderboard";
+   ranking_single.style.display = 'table';
+   ranking_team.style.display = 'none';
+
+}
+
+
+function Show_rank_team()
+{
+   var ranking_single = document.querySelector('#ranking_single');
+   var ranking_team = document.querySelector('#ranking_team');
+
+   var message = document.getElementById('mensaje');
+
+   message.innerHTML = "Random map Team - Leaderboard";
+   ranking_single.style.display = 'none';
+   ranking_team.style.display = 'table';
+
+}
+
+
